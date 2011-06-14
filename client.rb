9f8cbaaf7bad93528f9ed30296450e4602d2ff6d
Jordan Sissel <jls@semicomplete.com>
2011-06-14 00:11:00 -0700

    - update client example to use zmq

--- client.rb
+++ client.rb
@@ -1,22 +1,17 @@
 require "rubygems"
+require "zmq"
 require "msgpack"
-require "socket"
-require "ap"
+require "awesome_print"
 
-host = ARGV[0]
-
-sockargs = []
-sockargs << Socket::AF_INET6 if host =~ /:/
-socket = UDPSocket.new(*sockargs)
-#socket.send(MessagePack.pack("Hello world"), 0, "localhost", 3000);
-
-p MessagePack.pack([ "foo", { "testing" => 33.4 }])
-
-1.upto(1) do |i|
-  #socket.send(MessagePack.pack({ "request" => "dance", "testing" => 33.4 }), 0, ARGV[0], ARGV[1].to_i);
-  #socket.send(MessagePack.pack({ "request" => "restart", "program" => "hello world", "instance" => 53}), 0, ARGV[0], ARGV[1].to_i);
-  socket.send(MessagePack.pack({ "request" => "restart", "program" => "hello world"}), 0, ARGV[0], ARGV[1].to_i);
-  data = socket.sysread(4096)
-  r = MessagePack.unpack(data)
-  ap r
-end
+ctx = ZMQ::Context.new(1)
+socket = ctx.socket(ZMQ::REQ);
+socket.connect("tcp://localhost:3333")
+#socket.setsockopt(ZMQ::HWM, 100);
+socket.send(
+  #MessagePack.pack({"request" => "jump", "foo" => "bar"})
+  #MessagePack.pack({"request" => "dance", "foo" => "bar"})
+  MessagePack.pack({"request" => "restart", "program" => "hello world"})
+)
+response = socket.recv
+ap MessagePack.unpack(response)
+socket.close
