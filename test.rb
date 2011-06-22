febd6e89286a5f397cc8703cea053b8311887ca2
Jordan Sissel <jls@semicomplete.com>
2011-06-22 01:47:49 -0700

    - ruby code for streaming logs

---
+++ test.rb
@@ -0,0 +1,15 @@
+require "rubygems"
+require "zmq"
+require "msgpack"
+require "awesome_print"
+
+ctx = ZMQ::Context.new(1)
+socket = ctx.socket(ZMQ::SUB);
+p :connect => socket.connect("tcp://127.0.0.1:3344")
+p :setsockopt => socket.setsockopt(ZMQ::SUBSCRIBE, "")
+
+while true
+  p :reading
+  msg = socket.recv
+  p :msg => msg
+end
